e56d8b72bb983801862ca58e00c0f4e4039d713c
Ben Klein <robobenklein@gmail.com>
2016-01-02 23:06:44 -0500

    Missed recursive option

--- create-release.sh
+++ create-release.sh
@@ -2,8 +2,8 @@
 type zip || ( echo "ZIP CLI not found" && exit 1 );
 CREATIONTIME=`date -Idate`;
 mkdir -p release;
-zip "release/flathud-blue.zip" flathudblue;
-zip "release/flathud-red.zip" flathudred;
-zip "release/customizations.zip" Customization/*;
+zip -r release/flathud-blue.zip flathudblue;
+zip -r release/flathud-red.zip flathudred;
+zip -r release/customizations.zip Customization/*;
 touch release/release.md;
 echo "Released $CREATIONTIME" >> release/release.md;
