1caa2136f1ec45a3bc35e4c1a19f5ef142e66a07
Ben Klein <robobenklein@gmail.com>
2016-01-02 22:50:30 -0500

    Wrong brackets >.<

--- create-release.sh
+++ create-release.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-type zip || { echo "ZIP CLI not found"; exit 1 };
+type zip || ( echo "ZIP CLI not found" && exit 1 );
 CREATIONTIME=`date -Idate`;
 mkdir -p release;
-zip release/flathud-blue.zip flathudblue;
-zip release/flathud-red.zip flathudred;
-zip release/customizations.zip Customization/*;
+zip "release/flathud-blue.zip" "flathudblue";
+zip "release/flathud-red.zip" "flathudred";
+zip "release/customizations.zip" Customization/*;
 touch release/release.md;
 echo "Released $CREATIONTIME" >> release/release.md;
 
